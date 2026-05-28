503a3fecb81a2fd0f90f00a9344b9d88276c9efc
mrso1701 <andresrodriguez172008@gmail.com>
2026-05-28 15:10:45 -0500

    feat(taller10): Demostracion de error en escritura sin usar override en clases derivadas

---
+++ src/Taller10/SistemaOperativo.java
@@ -0,0 +1,7 @@
+package Taller10;
+
+public class SistemaOperativo {
+    public void iniciarSistema() {
+        System.out.println("Iniciando sistema base...");
+    }
+}
---
+++ src/Taller10/Windows.java
@@ -0,0 +1,10 @@
+package Taller10;
+
+
+public class Windows {
+    // Intento de sobrescritura SIN usar @Override
+    // Nota el error de tipeo intencional en el nombre (inicar en vez de iniciar)
+    public void inicarSistema() {
+        System.out.println("Iniciando Windows...");
+    }
+}
