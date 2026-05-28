15cb12d5273d54680d7ad2ad424ec0f46cba3476
mrso1701 <andresrodriguez172008@gmail.com>
2026-05-28 14:55:34 -0500

    feat(taller10): Instanciamiento de objetos en la clase main para probar polimorfismo

--- src/Taller10/PruebaTaller10.java
+++ src/Taller10/PruebaTaller10.java
@@ -3,7 +3,19 @@ package Taller10;
 public class PruebaTaller10 {
 
     public static void main(String[] args) {
+        // La variable es de tipo Persona, pero el objeto instanciado es diferente
+        Persona persona1 = new Persona();
+        Persona persona2 = new Estudiante();
+        Persona persona3 = new Profesor();
         
+        System.out.println("--- Presentaciones ---");
+        
+        // Java sabe cual metodo ejecutar basandose en la instancia real del objeto
+        persona1.presentarse(); // Ejecuta el de Persona
+        persona2.presentarse(); // Ejecuta el de Estudiante
+        persona3.presentarse(); // Ejecuta el de Profesor
+        
+        System.out.println("----------------------");
     }
     
 }
